6ee86606ce2682472f67cd186734a2613a51b8d0
agda <agomezdelamo@gmail.com>
2018-11-28 01:55:41 +0100

    configuracion jpa

---
+++ src/main/java/com/microservices/backendninja/entitys/Course.java
@@ -0,0 +1,85 @@
+package com.microservices.backendninja.entitys;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "curso")
+public class Course {
+
+	
+	@Column(name="id")
+	@Id //indica que es primary key
+	@GeneratedValue //hibernate genera el value
+	private int id;// hibernate no se lleva bien con columnas que tengan mayusculas
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="price")
+	private int price;
+	
+	@Column(name="hours")
+	private int hours;
+
+	public Course() {
+	}
+
+	// si tenemos un constructor personalizado, hay que crear el constructor por
+	// defecto, porque es el que usa hibenate
+	public Course(int id, String name, String description, int price, int hours) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.hours = hours;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+}
